f4289e7d93045573a224965efa99cd4b94db433d
Kim <kimvandenberg99@gmail.com>
2018-10-27 15:08:44 +0200

    Gui fix #123021932192103

--- src/presentation/GUI.java
+++ src/presentation/GUI.java
@@ -331,6 +331,7 @@ public class GUI implements Runnable {
     private JComboBox cbDeleteWatchedMediaProfile;
     private JComboBox cbDeleteWatchedMediaTitle;
     private JButton btnDeleteWatchBehaviour;
+    private JTextField textField1;
 
 
 //    Getters ------------------------------------------------------------------------------------------------------------------
